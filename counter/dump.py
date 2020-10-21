1f0b237beffe59002ec47ab6992e8f48090ae86c
Neeraj <neeraj.polas2@gmail.com>
2020-10-20 21:32:50 -0700

    added dump.py file

---
+++ counter/dump.py
@@ -0,0 +1,12 @@
+import requests
+import sys
+url = sys.argv[1]
+response = requests.get(url)
+keysJson = response.json()
+keys = keysJson.get('keys')
+for key in keys:
+    response2 = requests.get(url + key)
+    print(response2.json())
+
+
+
